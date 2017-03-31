778e11e1d10236573c89e2576284d37f3fb71dfb
paweloiii <3na10@1mi.pl>
2017-03-31 13:33:20 +0200

    Dodano auto-refresh

--- międzymordzie/pomiary/views.py
+++ międzymordzie/pomiary/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from .models import Pomiar, Sonda
+import json
 
 def index(request):
     context = {}
@@ -11,7 +12,8 @@ def index(request):
     return render(request, 'pomiary/index.html', context)
 
 def live_update(request):
-    data = {
-        'value': '30'
-    }
-    return JsonResponse(data);
+    dict = {}
+    for sonda in Sonda.objects.all():
+        dict[sonda.__str__()] = {'value': sonda.pomiary()[-1].wynik, 'time':{'godzina': sonda.pomiary()[-1].godzina(), 'data': sonda.pomiary()[-1].data.day}}
+
+    return JsonResponse(dict)
