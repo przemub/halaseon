b063a9c50e301c4488d0d670ebe3e98db2495e50
paweloiii <3na10@1mi.pl>
2017-03-31 14:02:54 +0200

    Poprawienie blednego sortowania sond

--- międzymordzie/pomiary/views.py
+++ międzymordzie/pomiary/views.py
@@ -4,16 +4,16 @@ from django.http import JsonResponse
 from .models import Pomiar, Sonda
 import json
 
-def index(request):
-    context = {}
+context = {}
 
+def index(request):
     context['sondy'] = [sonda for sonda in Sonda.objects.all()]
 
     return render(request, 'pomiary/index.html', context)
 
 def live_update(request):
     dict = {}
-    for sonda in Sonda.objects.all():
+    for sonda in context['sondy']:
         dict[sonda.__str__()] = {'value': sonda.pomiary()[-1].wynik, 'time':{'godzina': sonda.pomiary()[-1].godzina(), 'data': sonda.pomiary()[-1].data.day}}
 
     return JsonResponse(dict)
